0acc68fefaa9355ad73ffdaac5cfa4b845703c1e
juanzuluaga91 <juanzuluaga91@gmail.com>
2020-03-23 16:43:02 -0500

    [fix]

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,5 @@
-# kills a process named killmenow
-
-exec { 'killmenow':
-  path   => '/usr/bin/',
-  command   => 'pkill -f ./killmenow',
+# Execute a command
+exec { 'kill-killmenow':
+  command => 'pkill killmenow',
+  path    => '/usr/bin';
 }
