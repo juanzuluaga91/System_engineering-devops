68a26a062b1011076e02b1ec95cdae1afeee04da
juanzuluaga91 <juanzuluaga91@gmail.com>
2020-05-21 10:06:29 -0500

    [Commit]

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,7 @@
+exec { 'sed -ie \'s/class-wp-locale.phpp/class-wp-locale.php/\' /var/www/html/wp-settings.php':
+  command => '/bin/sed -ie \'s/class-wp-locale.phpp/class-wp-locale.php/\' /var/www/html/wp-settings.php',
+}
+
+exec { 'service apache2 restart':
+  command => '/usr/sbin/service apache2 restart',
+}
