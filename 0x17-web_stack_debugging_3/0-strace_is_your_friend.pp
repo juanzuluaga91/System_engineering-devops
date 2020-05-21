bdcf03e2c7bfd88a4f7e57f8ee04e4cb7c0a8624
juanzuluaga91 <juanzuluaga91@gmail.com>
2020-05-21 10:09:45 -0500

    [fix]

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,3 +1,4 @@
+# fix
 exec { 'sed -ie \'s/class-wp-locale.phpp/class-wp-locale.php/\' /var/www/html/wp-settings.php':
   command => '/bin/sed -ie \'s/class-wp-locale.phpp/class-wp-locale.php/\' /var/www/html/wp-settings.php',
 }
