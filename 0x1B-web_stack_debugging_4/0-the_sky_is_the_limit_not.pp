cfca58b3de37126dedf9c046606a977c4ed9ad90
juanzuluaga91 <juanzuluaga91@gmail.com>
2020-05-27 19:40:08 -0500

    [Commit]

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,12 @@
+# comment
+exec {'ulimit':
+  command => 'sed -r -i "s/(ULIMIT=\"-n) [0-9]+/\1 8000/" /etc/default/nginx',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+  onlyif  => 'test -e /etc/default/nginx',
+}
+
+exec {'restart nginx':
+  command => 'service nginx restart',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+  require => Exec['ulimit'],
+}
